1d544d7b65a94d76948e7553482cb7ab1699198c
Tosn <taoserking@163.com>
2022-04-13 16:52:59 +0800

    fix: route query highlight

--- src/layout/menu/SideMenu.vue
+++ src/layout/menu/SideMenu.vue
@@ -37,7 +37,7 @@ const route = useRoute();
 const menuStore = useMenuStore();
 
 const getDefaultActive = () => {
-  defaultActive.value = route.fullPath;
+  defaultActive.value = route.path;
 };
 onBeforeMount(() => {
   getDefaultActive();
